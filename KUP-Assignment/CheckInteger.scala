bb5a0b087dc14691199617eb372facca65eb5734
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-03-07 15:58:19 +0530

    Practice Exercise Added

---
+++ KUP-Assignment/CheckInteger.scala
@@ -0,0 +1,22 @@
+import scala.io.StdIn
+object Main {
+
+   def integerCheck(userInput:String): Boolean = {
+     var count = 0
+     for (index <- 0 until userInput.length) {
+       if (57 >= userInput.charAt(index).toInt && userInput.charAt(index).toInt >= 48) {
+         count=count+1
+       }
+     }
+     if(count != 0){
+        false
+     }else{
+        true
+     }
+   }
+  def main(args: Array[String]): Unit = {
+    println("Enter any String: ")
+    var userInput = StdIn.readLine()
+    println(integerCheck(userInput))
+  }
+}
