2f58d9e063a2e0c43c81e4370a3674774f394cba
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-03-17 11:31:03 +0530

    Session-2-Assignment-2 Added

--- KUP-Assignment/Session2Assignment2/src/main/scala/ExpandFunctions.scala
+++ KUP-Assignment/Session2Assignment2/src/main/scala/ExpandFunctions.scala
@@ -1,35 +1,24 @@
 object ExpandFunctions extends App {
-  // 1 -> Function for adding two number which takes two parameter of integer type and return integer type(SIMPLE FUNCTION)
-  val addFunction: (Int, Int) => Int = (number1: Int, number2: Int) => number1 + number2
-  println(addFunction(4, 1))
+  //1 -> val addFunction: (Int, Int) => Int = (number1: Int, number2: Int) => number1 + number2 (SIMPLE FUNCTION)
 
-  //Expanded function of adding two number
+  //Expanded function of above function
   val expandedAdd: Function2[Int, Int, Int] = new Function2[Int, Int, Int] {
-    def apply(number1: Int, number2: Int): Int = number1 + number2
+    override def apply(number1: Int, number2: Int): Int = number1 + number2
   }
-    println(expandedAdd.apply(3, 4))
-  // 2 -> Function for finding sum of elements of list of type list and  return integer type(SIMPLE FUNCTION)
-  val sumOfList: List[Int] => Int = (list: List[Int]) => list.sum
-  println(sumOfList(List(4,8,9)))
+
+  //2 -> val sumOfList: List[Int] => Int = (list: List[Int]) => list.sum (SIMPLE FUNCTION)
 
   // Expanded form of above sum function
-  val expandedSumOfList: Function1[List[Int], Int] = new Function1[List[Int], Int]  {
-    def apply(list: List[Int]): Int = list.sum
+  val expandedSumOfList: Function1[List[Int], Int] = new Function1[List[Int], Int] {
+    override def apply(list: List[Int]): Int = list.sum
   }
-  println(expandedSumOfList.apply(List(3, 4)))
-
-  // 3 ->Higher order Function for multiplying  which takes function and integer as a type and  return integer type(SIMPLE FUNCTION)
-  val higherOrderFunction: (Int => Int, Int) => Int = (storeFunction: (Int => Int), num: Int) => calculationFunction(num)
-  val calculationFunction: Int => Int = _ * 10
-  println(higherOrderFunction(calculationFunction, 17))
+  //3 -> val higherOrderFunction: (Int => Int, Int) => Int = (storeFunction: (Int => Int), num: Int) => calculationFunction(num) (SIMPLE FUNCTION)
 
   //Expanded form for above function
   val expandedHigherOrderFunction: Function2[Int => Int, Int, Int] = new Function2[Int => Int, Int, Int] {
-    def apply(storeFunction: Int => Int, number: Int): Int = storeFunction(number)
+    override def apply(storeFunction: Int => Int, number: Int): Int = storeFunction(number)
   }
   val calculationOfExpandedFunction: Function1[Int, Int] = new Function1[Int, Int] {
     def apply(number: Int): Int = number * 10
   }
-  println(higherOrderFunction.apply(calculationOfExpandedFunction, 8))
-
 }
--- KUP-Assignment/Session2Assignment2/src/test/scala/Session2Assignment2Test.scala
+++ KUP-Assignment/Session2Assignment2/src/test/scala/Session2Assignment2Test.scala
@@ -1,16 +1,20 @@
 import org.scalatest.funsuite.AnyFunSuite
 class Session2Assignment2Test extends AnyFunSuite {
-  val addFunction: (Int, Int) => Int = (number1: Int, number2: Int) => number1 + number2
-  val sumOfList: List[Int] => Int = (list: List[Int]) => list.sum
-  val higherOrderFunction: (Int => Int, Int) => Int = (storeFunction: (Int => Int), number: Int) => storeFunction(number)
-
   test("Addition of integers with") {
-        assert(addFunction(2,4) === 6)
+    val add: Int = ExpandFunctions.expandedAdd(2,4)
+    val expectedResult = 6
+    assert(add === expectedResult)
   }
   test("Finding sum of elements of list") {
-    assert(sumOfList(List(1,2,3,4,5)) === 15)
+    val sumOfList: Int = ExpandFunctions.expandedSumOfList(List(2,4,6))
+    val expectedResult = 12
+    assert(sumOfList === expectedResult)
   }
   test("Higher order function for multiplication") {
-    assert(higherOrderFunction((number:Int) => number * 10,9) === 90)
+    def calculationOfExpandedFunction(number: Int): Int = {
+      number * 10
+    }
+    val higherOrderFunction: Int = ExpandFunctions.expandedHigherOrderFunction(calculationOfExpandedFunction,9)
+    assert(calculationOfExpandedFunction(higherOrderFunction) === 90)
   }
 }
