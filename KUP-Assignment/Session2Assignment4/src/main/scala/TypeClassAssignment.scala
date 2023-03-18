263c4c4307dc79d5a977e749e5531c88bee249e5
vanshika1221 <vanshika.srivastava@knoldus.com>
2023-03-18 14:55:03 +0530

    Session 2 Assignment Added

--- KUP-Assignment/Session2Assignment4/src/main/scala/TypeClassAssignment.scala
+++ KUP-Assignment/Session2Assignment4/src/main/scala/TypeClassAssignment.scala
@@ -1,19 +1,23 @@
 trait Show[A] {
   def show(valueType: A): String
 }
-object TypeClassAssignment extends App {
-
-  implicit val doubleShow: Show[Double] = new Show[Double] {
-    def show(value: Double): String = value.toString
+class IntFunction[A] {
+  val intShow: Show[Int] = new Show[Int] {
+    override def show(intValue: Int): String = intValue.toString
   }
-
-  implicit val longShow: Show[Long] = new Show[Long] {
-    def show(value: Long): String = value.toString
+}
+class LongFunction[A] {
+  val longShow: Show[Long] = new Show[Long] {
+    override def show(longValue: Long): String = longValue.toString
   }
-
-  case class Position(xQuadrinate: Int, yQuadrinate: Int)
-  implicit val positionShow: Show[Position] = new Show[Position] {
-    def show(positionValue: Position) = s"Pos(x: ${positionValue.xQuadrinate}, y: ${positionValue.yQuadrinate})"
+}
+class DoubleFunction[A] {
+  val doubleShow: Show[Double] = new Show[Double] {
+    override def show(doubleValue: Double): String = doubleValue.toString
   }
-
+}
+case class Position(xQuardinate: Int, yQuardinate: Int) {
+  val positionShow: Show[Position] = new Show[Position] {
+    override def show(positionValue: Position): String = s"Position(x: ${positionValue.xQuardinate} , y: ${positionValue.yQuardinate})"
+  }
 }
--- KUP-Assignment/Session2Assignment4/src/test/scala/Session2Assignment4Test.scala
+++ KUP-Assignment/Session2Assignment4/src/test/scala/Session2Assignment4Test.scala
@@ -1,25 +1,21 @@
 import org.scalatest.funsuite.AnyFunSuite
 class Session2Assignment4Test extends AnyFunSuite {
-  test("Test case 1") {
-    val doubleshowValue = doubleShow.show(56.0).toString
-    val expected = "56.0"
-    assert(doubleshowValue === expected)
+  test("Test case for Int") {
+    val intShowValue = new IntFunction
+    assert(intShowValue.intShow.show(30) === "30")
   }
-  test("Test case 2") {
-    val intShowValue = intShow.show(56)
-    val expected = "56"
-    assert(intShowValue === expected)
+  test("Test case for long") {
+    val longShowValue = new LongFunction
+    assert(longShowValue.longShow.show(6580080088L) === "6580080088")
   }
-
-  test("Test case 3") {
-    val longShowValue = longShow.show(569009090099L)
-    val expected = "569009090099"
-    assert(longShowValue === expected)
+  test("Test case for double") {
+    val doubleShowValue = new DoubleFunction
+    assert(doubleShowValue.doubleShow.show(5123233.92) === "5123233.92")
   }
-  test("Test case 4") {
-    val position = new Position(0, 1)
-    val positionString = positionShow.show(position)
-    val expected = "Pos(x: 0, y: 1)"
-    assert(positionString === expected)
+  test("Test case for case class") {
+    val position = Position(0,1)
+    val positionShowValue = position.positionShow.show(position)
+    assert(positionShowValue === "Position(x: 0 , y: 1)")
   }
 }
+
